aa15d93922e27fadb8264beff0537decb822aaf7
Thomas Gander (Home) <Thomas.Gander@mymhc.ca>
2023-01-08 19:34:30 -0700

    Finished needs comments

--- ITEC145-Section A/Form1.Designer.cs
+++ ITEC145-Section A/Form1.Designer.cs
@@ -31,11 +31,24 @@
             this.btnGenerate = new System.Windows.Forms.Button();
             this.lstOutput = new System.Windows.Forms.ListBox();
             this.btnSum = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtStartSubscript = new System.Windows.Forms.TextBox();
+            this.txtValues = new System.Windows.Forms.TextBox();
+            this.lstIndex = new System.Windows.Forms.ListBox();
+            this.lstValues = new System.Windows.Forms.ListBox();
+            this.btnShowValues = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnGenerate
             // 
-            this.btnGenerate.Location = new System.Drawing.Point(12, 12);
+            this.btnGenerate.Location = new System.Drawing.Point(12, 561);
             this.btnGenerate.Name = "btnGenerate";
             this.btnGenerate.Size = new System.Drawing.Size(75, 23);
             this.btnGenerate.TabIndex = 0;
@@ -49,12 +62,12 @@
             this.lstOutput.ItemHeight = 15;
             this.lstOutput.Location = new System.Drawing.Point(12, 41);
             this.lstOutput.Name = "lstOutput";
-            this.lstOutput.Size = new System.Drawing.Size(156, 319);
+            this.lstOutput.Size = new System.Drawing.Size(156, 514);
             this.lstOutput.TabIndex = 1;
             // 
             // btnSum
             // 
-            this.btnSum.Location = new System.Drawing.Point(93, 12);
+            this.btnSum.Location = new System.Drawing.Point(93, 561);
             this.btnSum.Name = "btnSum";
             this.btnSum.Size = new System.Drawing.Size(75, 23);
             this.btnSum.TabIndex = 2;
@@ -62,17 +75,146 @@
             this.btnSum.UseVisualStyleBackColor = true;
             this.btnSum.Click += new System.EventHandler(this.btnSum_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(426, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Array Starting Subscript:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(426, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "How Many Index\'s to Show?";
+            // 
+            // txtStartSubscript
+            // 
+            this.txtStartSubscript.Location = new System.Drawing.Point(588, 38);
+            this.txtStartSubscript.Name = "txtStartSubscript";
+            this.txtStartSubscript.Size = new System.Drawing.Size(100, 23);
+            this.txtStartSubscript.TabIndex = 5;
+            // 
+            // txtValues
+            // 
+            this.txtValues.Location = new System.Drawing.Point(588, 83);
+            this.txtValues.Name = "txtValues";
+            this.txtValues.Size = new System.Drawing.Size(100, 23);
+            this.txtValues.TabIndex = 6;
+            // 
+            // lstIndex
+            // 
+            this.lstIndex.FormattingEnabled = true;
+            this.lstIndex.ItemHeight = 15;
+            this.lstIndex.Location = new System.Drawing.Point(174, 41);
+            this.lstIndex.Name = "lstIndex";
+            this.lstIndex.Size = new System.Drawing.Size(120, 514);
+            this.lstIndex.TabIndex = 7;
+            // 
+            // lstValues
+            // 
+            this.lstValues.FormattingEnabled = true;
+            this.lstValues.ItemHeight = 15;
+            this.lstValues.Location = new System.Drawing.Point(300, 41);
+            this.lstValues.Name = "lstValues";
+            this.lstValues.Size = new System.Drawing.Size(120, 514);
+            this.lstValues.TabIndex = 8;
+            // 
+            // btnShowValues
+            // 
+            this.btnShowValues.Location = new System.Drawing.Point(426, 114);
+            this.btnShowValues.Name = "btnShowValues";
+            this.btnShowValues.Size = new System.Drawing.Size(97, 23);
+            this.btnShowValues.TabIndex = 9;
+            this.btnShowValues.Text = "Show Values!";
+            this.btnShowValues.UseVisualStyleBackColor = true;
+            this.btnShowValues.Click += new System.EventHandler(this.btnShowValues_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(174, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 15);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Array Index";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(300, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 15);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Array Value";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(694, 41);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 15);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "(0 to 29)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(644, 86);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 15);
+            this.label6.TabIndex = 13;
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(148, 15);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Generated Numbers / Sum";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 23);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(55, 15);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "For Array";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 590);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnShowValues);
+            this.Controls.Add(this.lstValues);
+            this.Controls.Add(this.lstIndex);
+            this.Controls.Add(this.txtValues);
+            this.Controls.Add(this.txtStartSubscript);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnSum);
             this.Controls.Add(this.lstOutput);
             this.Controls.Add(this.btnGenerate);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -81,5 +223,18 @@
         private Button btnGenerate;
         private ListBox lstOutput;
         private Button btnSum;
+        private Label label1;
+        private Label label2;
+        private TextBox txtStartSubscript;
+        private TextBox txtValues;
+        private ListBox lstIndex;
+        private ListBox lstValues;
+        private Button btnShowValues;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
     }
 }
--- ITEC145-Section A/Form1.cs
+++ ITEC145-Section A/Form1.cs
@@ -42,5 +42,51 @@ namespace ITEC145_Section_A
             lstOutput.Items.Clear();    //clears listbox
             lstOutput.Items.Add(sum);   //adds sum variable to listbox
         }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnShowValues_Click(object sender, EventArgs e)
+        {
+            
+
+            try
+            {
+                int subscript = int.Parse(txtStartSubscript.Text);
+                int numberOfValues = int.Parse(txtValues.Text);
+
+                if (subscript > 29 || numberOfValues > (30 - subscript))
+                {
+                    MessageBox.Show("Hi Steve, please stop trying to break my program :(");
+                    MessageBox.Show("Just enter values that are within the bounds of my array...");
+                    lstIndex.Items.Clear();
+                    lstValues.Items.Clear();
+                }
+                else
+                {
+                    lstIndex.Items.Clear();
+                    lstValues.Items.Clear();
+
+                    for (int i = subscript; i < subscript + numberOfValues; i++)
+                    {
+                        lstIndex.Items.Add(i);
+                        lstValues.Items.Add(ints[i]);
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Please enter valid integers between 0-29!");
+                lstIndex.Items.Clear();
+                lstValues.Items.Clear();
+            }
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
