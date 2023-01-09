52e3d7929a5cef52cdfc7702c2a7148076961838
Thomas Gander (Home) <Thomas.Gander@mymhc.ca>
2023-01-08 17:41:44 -0700

    Finished Part 1 with comments

--- ITEC145-Section A/Form1.cs
+++ ITEC145-Section A/Form1.cs
@@ -4,9 +4,9 @@ namespace ITEC145_Section_A
 
     public partial class Form1 : Form
     {
-        int[] ints = new int[30];
-        Random random = new Random();
-        int count = 0;
+        int[] ints = new int[30];           //Array to hold ints
+        Random random = new Random();       //Random variable
+        int count = 0;                      //Counter variable for do-while loop
         public Form1()
         {
             InitializeComponent();
@@ -14,33 +14,33 @@ namespace ITEC145_Section_A
 
         private void btnGenerate_Click(object sender, EventArgs e)
         {
-            lstOutput.Items.Clear();
-            count = 0;
+            lstOutput.Items.Clear();                //Clears listbox before filling it.
+            count = 0;                              //Initializes count variable.
 
             for(int i = 0; i < ints.Length; i++)
             {
-                ints[i] = random.Next(0, 101);
+                ints[i] = random.Next(0, 101);      //For loop to fill each integer location in ints array
             }
             
 
             do
             {
-                lstOutput.Items.Add(ints.GetValue(count));
-                count++;
+                lstOutput.Items.Add(ints.GetValue(count));      //Gets the value of each integer in each location using count variable
+                count++;    //increments count each time do runs
             } 
-            while (count < 30);
+            while (count < ints.Length);     //do iterates until count hits ints array length
         }
 
         private void btnSum_Click(object sender, EventArgs e)
         {
-            int sum = 0;
+            int sum = 0;        //intialises sum variable
 
-            foreach(int i in ints)
+            foreach(int i in ints)      //foreach to add each value stored in the array to sum
             {
                 sum += i;
             }
-            lstOutput.Items.Clear();
-            lstOutput.Items.Add(sum);
+            lstOutput.Items.Clear();    //clears listbox
+            lstOutput.Items.Add(sum);   //adds sum variable to listbox
         }
     }
 }
